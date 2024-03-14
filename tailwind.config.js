c4c3a409f7bd404479743252d7ed2c223514350a
sabalearning01 <sabatemitayo44@gmail.com>
2024-03-14 15:33:48 +0100

    ODS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'bg': "url('public/img/People at the office during a work day.svg')",
+      }
+
+    },
+    },
+    plugins: [],
+  }
+
