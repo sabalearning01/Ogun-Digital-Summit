5f937dc736e10f9da974070a37b7ccc9eff3771b
sabalearning01 <sabatemitayo44@gmail.com>
2024-03-20 12:53:29 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,22 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {
+  extend: {
+    screens: {
+        'smll': '320px',
+        'sm': '640px',
+        'point': '643px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
       backgroundImage:{
         'bg': "url('public/img/People at the office during a work day.svg')",
+
       }
 
     },
     },
     plugins: [],
-  }
+  };
 
